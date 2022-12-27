0608d99e67918dfa6f5d89e229235eae92650958
Larisse Rodrigues <larisse.rodrigues@g.globo>
2022-12-27 10:19:20 -0300

    atualizações incluindo item lista e removendo item adicionado

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,84 @@
-const form = document.getElementById("novoItem");
-const lista = document.getElementById("lista");
-const itens = []
+
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach((elemento) => {
+  criaElemento(elemento)
+})
 
 form.addEventListener("submit", (evento) => {
   //O método preventDefault() cancela o evento se for cancelável, significando que a ação padrão que pertence ao evento não ocorrerá.
-  evento.preventDefault();
+  evento.preventDefault()
 
-  const nome = evento.target.elements["nome"];
-  const quantidade = evento.target.elements["quantidade"];
+  const nome = evento.target.elements['nome']
+  const quantidade = evento.target.elements['quantidade']
 
-  //submit chama funcao cria elemento
-  criaElemento(nome.value, quantidade.value);
+  //Com o método find(), ele procura um elemento e, com o operador de comparação ===, ele compara se o valor e tipo de dois elementos são idênticos.
+  const existe = itens.find(elemento => elemento.nome === nome.value)
 
-  nome.value = "";
-  quantidade.value = "";
-
-});
-
-//função com parametro para retornar dados
-function criaElemento(nome, quantidade) {
-  const novoItem = document.createElement("li");
-  novoItem.classList.add("item");
-
-  const numeroItem = document.createElement("strong");
-  numeroItem.innerHTML = quantidade;
-
-  novoItem.appendChild(numeroItem);
-  novoItem.innerHTML += nome;
-
-  lista.appendChild(novoItem);
-
-  //
   const itemAtual = {
-    "nome" : nome,
-    "quantidade" : quantidade
+    "nome": nome.value,
+    "quantidade": quantidade.value
   }
 
-  //inserir um elemento no array
-  itens.push(itemAtual)
+  if (existe) {
+    itemAtual.id = existe.id
 
-  //localStorage = local para armazenamento de informações
-  //json.stringify. stringify = Serve para transformar esse elemento em uma string
-  localStorage.setItem("item", JSON.stringify(itens))
+    atualizaElemento(itemAtual)
+
+    itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
+  } else {
+    //se itens existir, pega ID e adiciona 1 para incrementar e atualizar o item do array
+    itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0;
+
+    criaElemento(itemAtual)
+    //inserir um elemento no array
+    itens.push(itemAtual)
+  }
+
+  localStorage.setItem("itens", JSON.stringify(itens))
+
+  nome.value = ""
+  quantidade.value = ""
+})
+
+//função com parametro para retornar dados
+function criaElemento(item) {
+  const novoItem = document.createElement("li")
+  novoItem.classList.add("item")
+
+  const numeroItem = document.createElement("strong")
+  numeroItem.innerHTML = item.quantidade
+  numeroItem.dataset.id = item.id
+  novoItem.appendChild(numeroItem)
+
+  novoItem.innerHTML += item.nome
+
+  novoItem.appendChild(botaoDeleta(item.id))
+
+  lista.appendChild(novoItem)
 }
 
+function atualizaElemento(item) {
+  document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id) {
+  const elementoBotao = document.createElement("button")
+  elementoBotao.innerText = "X"
+
+  elementoBotao.addEventListener("click", function () {
+    deletaElemento(this.parentNode, id)
+  })
+
+  return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+  tag.remove()
+
+  itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+  //json.stringify. stringify = Serve para transformar esse elemento em uma string
+  localStorage.setItem("itens", JSON.stringify(itens))
+}
