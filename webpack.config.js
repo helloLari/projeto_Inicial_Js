6aa3954ba72819c444fadb667639bfdb14b9092f
Larisse Rodrigues <larisse.rodrigues@g.globo>
2023-01-18 18:06:23 -0300

    webpack incluído ao projeto

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = ({ mode = 'development' }) => ({
+  mode,
+  entry: './src/main.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+})
